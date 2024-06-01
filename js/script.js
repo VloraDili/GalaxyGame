47ada6e3a9339d63e0d36f04c2fc1f64bf4e5816
Fatlinda0 <fb55487@ubt-uni.net>
2024-06-02 00:50:36 +0200

    Fatlinda

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+    var body = document.body;
+    var darkModeToggle = document.getElementById('dark-mode-toggle');
+        
+    var isDarkMode = body.classList.contains('dark-mode');
+    
+    if (isDarkMode) {
+        body.classList.remove('dark-mode');
+        darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+    } else {
+        body.classList.add('dark-mode');
+        darkModeToggle.innerHTML = '<span class="sun-icon"><i class="fas fa-sun"></i></span>';
+    }
+}
+
+let stars = document.getElementById('stars');
+let person = document.getElementById('person');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let galaxy_front = document.getElementById('galaxy_front');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.3 + 'px';
+    person.style.marginLeft = value * 0.4 + 'px';
+    text.style.marginRight = value * 0.8 + 'px';
+    btn.style.marginTop = value * 0.5 + 'px';
+})
