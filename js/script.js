22abcae7e6405840cb0e38129717c5fbc86ab690
FatlindaBerisha <96177179+Fatlinda0@users.noreply.github.com>
2024-06-12 23:03:53 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
+var loader = document.getElementById("preloader");
+function hidePreloader() {
+    loader.style.display = "none";
+}
+window.addEventListener("load", function(){
+    setTimeout(hidePreloader, 1000);
+});
+
 function toggleMode() {
-    var body = document.body;
-    var darkModeToggle = document.getElementById('dark-mode-toggle');
-        
-    var isDarkMode = body.classList.contains('dark-mode');
-    
-    if (isDarkMode) {
-        body.classList.remove('dark-mode');
-        darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
-    } else {
-        body.classList.add('dark-mode');
-        darkModeToggle.innerHTML = '<span class="sun-icon"><i class="fas fa-sun"></i></span>';
-    }
+  var body = document.body;
+  var darkModeToggle = document.getElementById('dark-mode-toggle');
+  var isDarkMode = body.classList.contains('dark-mode');
+
+  if (isDarkMode) {
+    body.classList.remove('dark-mode');
+    darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+  } else {
+    body.classList.add('dark-mode');
+    darkModeToggle.innerHTML = '<span class="sun-icon"><i class="fas fa-sun"></i></span>';
+  }
 }
 
 let stars = document.getElementById('stars');
